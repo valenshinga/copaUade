37ec39f898de917e57fabe2340347181e9e563e5
Emanuel Strah <manustrah@gmail.com>
2025-05-06 16:51:32 -0300

    nombres de variables control de errores eligiendo

--- desafio1/main.py
+++ desafio1/main.py
@@ -3,8 +3,8 @@ import os
 
 # abre y guarda en memoria las preguntas y respuestas del csv
 def abrir_csv(csvPath) -> None:
-    directorio_actual = os.path.dirname(os.path.abspath(__file__)) 
-    fullPath = os.path.join(directorio_actual, csvPath)
+    absolutePath = os.path.dirname(os.path.abspath(__file__)) 
+    fullPath = os.path.join(absolutePath, csvPath)
     memoria = dict()    #un diccionario para tener en memoria
     #registrar las preguntas y respuestas en el diccionario
     with open(fullPath,"r", newline="",encoding='utf-8') as csvfile:
@@ -23,15 +23,16 @@ def mostrar_opciones(memoria:dict)->list:
         print(f"-> {index + 1} : {key}")
     return opciones
         
+#solicita una respuesta a la pregunta y la agrega al csv
 def registrar_pregunta(memoria:dict, pregunta:str, csvPath:str) -> None:
-    respuestaPregunta = input("-> ¿Cual sería la respuesta esperada para esa pregunta?: ")
-    directorio_actual = os.path.dirname(os.path.abspath(__file__)) 
-    fullPath = os.path.join(directorio_actual, csvPath)
+    respuesta = input("-> ¿Cual sería la respuesta esperada para esa pregunta?: ")
+    absolutePath = os.path.dirname(os.path.abspath(__file__)) 
+    fullPath = os.path.join(absolutePath, csvPath)
 
     with open(fullPath,"a", newline="",encoding='utf-8') as csvfile: #registra la respuesta a la pregunta
         csvWriter = csv.writer(csvfile)
-        csvWriter.writerow([pregunta,respuestaPregunta])
-    memoria[pregunta] = respuestaPregunta
+        csvWriter.writerow([pregunta,respuesta])
+    memoria[pregunta] = respuesta
     print("-> respuesta registrada, ¡gracias!")
 
 def main_loop(csvPath:str) -> None: 
@@ -40,22 +41,32 @@ def main_loop(csvPath:str) -> None:
     print("-> ¡Hola! Soy un asistente virtual, ¿en qué puedo ayudarte?")
     print("-> Si no sabés qué preguntar, podés ver las preguntas disponibles escribiendo 'opciones'")
     while True:     
-        r = input("""-> Ingrese una pregunta o "opciones" (ingrese "salir" para salir): """)
-        if r in {"salir","SALIR","Salir","exit"}: break # si quiere salir
-        elif r == "opciones": #si elije mostrar las opcines
+        respuesta = input("""-> Ingrese una pregunta o "opciones" (ingrese "salir" para salir): """)
+        if respuesta in {"salir","SALIR","Salir","exit"}: break # si quiere salir
+        elif respuesta == "opciones": #si elije mostrar las opcines
             opciones = mostrar_opciones(memoria)
-            opcionElegida = int(input("Ingrese el numero de la pregunta: "))
-            while opcionElegida <= 0 or opcionElegida > len(opciones):
-                opcionElegida = int(input("Opcion invalida. Ingrese el numero de la pregunta: "))
+            opcionElegida = 0
+            while True:
+                opcionElegida = input("Ingrese el numero de la pregunta: ")
+                try: #controla que ingrese un numero dentro del rango
+                    opcionElegida = int(opcionElegida)
+                    if opcionElegida <= 0 or opcionElegida > len(opciones):
+                        print("Opcion fuera de rango.",end="")
+                    else:
+                        break
+                except Exception as e:
+                    print("Opcion invalida.",end="")
+                    opcionElegida = 0
             print(f"-> La respuesta a la pregunta {opciones[opcionElegida - 1]} es:")
             print(f"-> {memoria[opciones[opcionElegida - 1]]}")
-        elif r in memoria: #si la pregunta esta registrada
-            print(f"-> {memoria[r]}")
+        elif respuesta in memoria: #si la pregunta esta registrada
+            print(f"-> {memoria[respuesta]}")
         else:   #si la pregunta NO esta registrada
-            r2 = input("-> No conozco esa pregunta ¿queres ingresar una respuesta a esa pregunta?: ")
-            if r2 in {"si","Si","sí","Sí","s","y"}: #si se quiere ingresar una respuesta para la pregunta no resgitrada
-                registrar_pregunta(memoria,r,csvPath)
-
+            respuesta2 = input("-> No conozco esa pregunta ¿queres ingresar una respuesta a esa pregunta?: ")
+            if respuesta2 in {"si","Si","sí","Sí","s","y"}: #si se quiere ingresar una respuesta para la pregunta no resgitrada
+                registrar_pregunta(memoria,respuesta,csvPath)
+            elif respuesta2 not in {"no","NO","No","n","N"}: #si se ingresa algo raro
+                print("-> Voy a asumir que eso es un no.")
     print("-> ¡Chau!")
 
 if __name__ == "__main__":
